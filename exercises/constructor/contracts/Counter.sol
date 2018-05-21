c0b3e850e800963ced0276f318aa4b9081feb28f
Elias <elias3c@gmail.com>
2018-05-21 12:36:36 -0500

    Complete hello-world, state-change, struct, and constructor

--- exercises/constructor/contracts/Counter.sol
+++ exercises/constructor/contracts/Counter.sol
@@ -2,6 +2,9 @@ pragma solidity 0.4.23;
 
 contract Counter {
     // declare public count variable (uint)
-
-    // create a constructor wich accepts initial value of the count variable
+    uint public count;
+    // create a constructor which accepts initial value of the count variable
+    constructor(uint _count) public {
+      count = _count;
+    }
 }
--- exercises/state-change/contracts/Counter.sol
+++ exercises/state-change/contracts/Counter.sol
@@ -2,8 +2,18 @@ pragma solidity 0.4.23;
 
 contract Counter {
     // define public count variable (uint) to store number of ticks
+    uint counter = 0;
+
+    function count() public view returns (uint) {
+      return counter;
+    }
 
     // implement tick() to increment counter
-
+    function tick() public {
+      counter++;
+    }
     // implement reset() to reset counter back to 0
+    function reset() public {
+      counter = 0;
+    }
 }
